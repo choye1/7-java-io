6308146ae985417ecb2d5579760d35747761ce36
choye1 <makcim233237@gmail.com>
2024-10-31 19:47:05 +0500

    Скоробогатов ПрИ-202 Task 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -8,14 +8,24 @@ public class Task01Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
-        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
+
+        //System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
+
 
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException("inputStream is null");
+        }
+
+        int checksum = 0;
+        int data;
+
+        while ((data = inputStream.read()) != -1) {
+            checksum = Integer.rotateLeft(checksum, 1) ^ data;
+        }
+
+        return checksum;
     }
 }
